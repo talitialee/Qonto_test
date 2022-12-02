db3755b7fa997fbe24ed99c4cefb1e878ee890ce
Alice Reuter <110020531+talitialee@users.noreply.github.com>
2022-12-02 22:23:18 +0100

    Premier ajout
    Upload de la base du site (pour entraînement personnel):
    index > menu + 2 sections (img + texte), quelques éléments de contenu
    style > focus sur le responsive, principalement du menu. Avec mediaqueries < 1024px : apparition du burger (icône menu) + possibilité de faire apparaître une liste de liens. La liste est séparée dans une autre div, car sinon ça ne marchait pas avec  style, js, menu responsive
    js > animation pour le menu (clic burger > apparition liste de liens) + fade in de l'image

---
+++ myscript.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.className === "navmobile") {
+      x.className += " responsive";
+    } else {
+      x.className = "navmobile";
+    }
+
+    
+  }
+
+  
+  $("#photo1").fadeIn("slow");
+  
+  $("#photo2").fadeIn("slow");
